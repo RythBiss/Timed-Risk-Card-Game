d00cdb6d8c60b796999dd08c0fcf105798f733d1
RythBiss <dkschaarschmidt@gmail.com>
2022-08-31 08:43:24 -0400

    Progress updates with player inputs and state changes. Changed buttons to 'Accept Risk' and 'Pass'; both buttons draw cards at the end of their function, but accept risk calculates the results of the gamble and updates the progress state. Added some game logic automation.
    Signed-off-by: RythBiss <dkschaarschmidt@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Deck from './Components/Deck';
 import Progressbar from './Components/Progressbar';
 
@@ -7,10 +7,24 @@ function App() {
 
   const [progress, setProgress] = useState(10);
 
+  const setProgressClamped = (num) => {
+    const newProgress = num + progress;
+    setProgress(Math.min(Math.max(newProgress, 0), 100));
+  }
+
+  useEffect(() => {
+    console.log(`progress: ${progress}`)
+    if(progress >= 100){
+      console.log('You won!');
+    }else if(progress <= 0){
+      console.log('You lost...');
+    }
+  }, [progress]);
+
   return (
     <div className="App">
       <Progressbar progress={progress}/>
-      <Deck />
+      <Deck updateProgress={setProgressClamped}/>
     </div>
   );
 }
--- src/Components/Deck.js
+++ src/Components/Deck.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Deck() {
+export default function Deck(props) {
 
-  //finish creating card class
-  class card {
-    constructor(success, fail){
-      this.success = 50;
-      this.fail = 50;
+  const [deck, setDeck] = useState([0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0.90,0.95]);
+  const [deckPos, setDeckPos] = useState(0);
+
+  //increment position in deck by 1
+  const draw = () => {
+    if((deckPos + 1) < (deck.length)){
+      setDeckPos(deckPos + 1);
+    }else{
+      shuffleDeckHook();
+      setDeckPos(0);
     }
   }
 
-  const [deck, setDeck] = useState([0,1,2,3,4,5,6,7,8,9]);
-  const [deckPos, setDeckPos] = useState(0);
-
-  const draw = () => {
-    setDeckPos(deckPos + 1);
+  const shuffleDeckHook = () => {
+    setDeck(shuffle([...deck]));
   }
 
+  //shuffle the passed array and return it
   const shuffle = (array) => {
     const length = array.length;
     let randomIndex = null;
     let tempArrayStorage = null;
-
-    console.log(`array before ${array}`);
 
     for(let i = 0; i < length; i++){
       //pick random index ahead of current one
       const min = Math.ceil(i);
       const max = Math.floor(length-1);
       randomIndex = Math.floor(Math.random() * (max - min) + min); 
-      console.log(randomIndex);
 
       //switch current and random index's content
       tempArrayStorage = array[randomIndex];
@@ -39,13 +39,42 @@ export default function Deck() {
 
     return array;
   }
-  
+
+  const rollResults = (success) => {
+    const roll = Math.random();
+
+    console.log(`Success if ${roll} is lower than ${success}`)
+
+    if(roll < success){
+      console.log('Success');
+      return true;
+    }else{
+      console.log('Failed...');
+      return false;
+    }
+  }
+
+  const takeRisk = () => {
+    const result = rollResults(deck[deckPos]);
+
+    if(result){
+      props.updateProgress(7.5);
+    }else{
+      props.updateProgress(-7.5);
+    }
+
+    draw();
+  }
+
+  useEffect(() => {
+    shuffleDeckHook();
+  }, []);
 
   return (
     <>
-      <button onClick={() => {setDeck(shuffle([...deck]))}}>shuffle</button>
-      <button onClick={draw}>draw</button>
-      <h1>Deck position: {deck[deckPos]}</h1>
+      <button onClick={takeRisk}>Accept Risk</button>
+      <button onClick={draw}>Pass</button>
+      <h1>Deck position: {deck[deckPos] *100}%</h1>
       <p>{deck}</p>
     </>
   )
